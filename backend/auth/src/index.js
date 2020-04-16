71e1162aff58965bba90aebf7feaa0e8d987b855
jutorres <juliana.torres@thoughtworks.com>
2020-04-15 21:02:55 -0300

    Login funcionando

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -3,15 +3,12 @@ var bodyParser = require('body-parser');
 var Keycloak = require('keycloak-connect');
 
 const kcConfig = {
-  realm: 'ConstrSW',
-  'auth-server-url': process.env.AUTH_URL,
-  'ssl-required': 'external',
-  'bearer-only': true,
-  resource: 'constrsw-auth',
-  credentials: {
-    secret: process.env.CLIENT_SECRET
-  },
-  'confidential-port': 0
+    "realm": "Test",
+    "auth-server-url": "http://host.docker.internal:8080/auth",
+    "ssl-required": "external",
+    "resource": "Test",
+    "public-client": true,
+    "confidential-port": 0
 };
 
 const keycloak = new Keycloak({}, kcConfig);
